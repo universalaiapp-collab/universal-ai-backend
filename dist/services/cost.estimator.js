b171cfed179e250ee22ecf29226e09cf4dc65e42
shubhankar <shubhankar143.sss@gmail.com>
2025-11-29 21:34:05 +0530

    chore: remove compiled dist and backups from repo index

--- dist/services/cost.estimator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.estimateCost = estimateCost;
-function estimateCost(provider, tokens) {
-    const rate = provider === 'openai' ? (process.env.OPENAI_COST_PER_TOKEN || 0.00002) : (process.env.GEMINI_COST_PER_TOKEN || 0.00002);
-    return Number(tokens) * Number(rate);
-}
-exports.default = { estimateCost };
--- src-backup-tmp/services/providerAdapters/types.ts
+++
@@ -1,8 +0,0 @@
-// src/services/providerAdapters/types.ts
-export type AdapterResponse = {
-  provider: string;
-  model: string;
-  text: string;
-  promptTokens: number;
-  completionTokens: number;
-};
